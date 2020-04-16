c70d43c61dcea8fecd4443c62829d7a74117e007
Vitor Hugo <vitor.h8@icloud.com>
2020-04-16 13:47:34 -0300

    Login Screen - Done

--- Login UI/ContentView.swift
+++ Login UI/ContentView.swift
@@ -10,7 +10,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Image("abstract")
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    VStack(alignment: .leading) {
+                        Image("logo")
+                            .resizable()
+                            .frame(width: 75, height: 75)
+                        
+                        Text("Hello,")
+                            .font(.custom("Helvetica Neue", size: 40))
+                            .fontWeight(.regular)
+                            .foregroundColor(.white)
+                        
+                        Text("Vitor")
+                            .font(.custom("Helvetica Neue", size: 40))
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                    }
+                    .padding([.top, .leading], 15.0)
+                    
+                    Spacer()
+                }
+                .padding([.leading, .bottom, .trailing], 20.0)
+                
+                Spacer()
+                
+                Button(action: { }, label: {
+                    Text("Sign Up")
+                        .font(.custom("Helvetica Neue", size: 20))
+                        .frame(width: 300, alignment: .center)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(RoundedRectangle(cornerRadius: .infinity).stroke(Color.white, lineWidth: 2))
+                })
+                    .padding()
+                
+                Button(action: { }, label: {
+                    Text("Sign In")
+                        .font(.custom("Helvetica Neue", size: 20))
+                        .frame(width: 300, alignment: .center)
+                        .padding()
+                        .foregroundColor(Color(.systemYellow))
+                        .background(Color.white)
+                        .cornerRadius(.infinity)
+                })
+                    .padding(.bottom)
+                
+                Button(action: {}, label: {
+                    Text("Forgot password?")
+                        .font(.custom("Helvetica Neue", size: 16))
+                        .fontWeight(.regular)
+                        .underline()
+                        .font(.body)
+                        .foregroundColor(.white)
+                        .padding()
+                })
+            }
+        }
     }
 }
 
